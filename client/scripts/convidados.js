f1a2b0497b4a278ce5d06e28ed877e3d9a2f0168
MARLON MIRANDA JUNIOR <05259876067@senacrs.edu.br>
2026-06-17 17:03:59 -0300

    Projeto sendo feito para um estudo pré-competição

---
+++ client/scripts/convidados.js
@@ -0,0 +1,30 @@
+function logout() {
+    const prosseguir = confirm('Deseja mesmo sair?')
+
+    if (prosseguir){
+        window.location.href = 'index.html'
+        localStorage.removeItem('token')
+    
+    }
+}
+
+if (!localStorage.getItem('token')){
+    alert('Faça o login novamente.')
+    window.location.href = 'index.html'
+}
+
+const sideBarToggle = document.getElementById('sideBarToggle')
+const sidebar = document.getElementById('sidebar')
+const nav = document.querySelector('nav')
+const titulo = document.getElementById('titulo')
+const main = document.querySelector('main')
+const botaoSair = document.getElementById('botaoSair')
+
+sidebar.addEventListener('click', () =>{
+    sidebar.classList.toggle('w-72')
+    nav.classList.toggle('hidden')
+    titulo.classList.toggle('hidden')
+    botaoSair.classList.toggle('hidden')
+    main.removeEventListener('click')
+})
+
